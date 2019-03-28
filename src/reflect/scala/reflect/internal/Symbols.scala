1e08d2f03e42f16b02793f79c6cc57e73dcd6e8f
Diego E. Alonso-Blas <diesalbla@gmail.com>
2019-03-28 01:34:37 +0000

    Add lastSuch method, use it to avoid List flatten.
    We add to the Collections trait a function "lastSuch", which returns
    the right-most element of the list that meets a property.
    We use this method to replace a call to `List.flatten.last`.

--- src/reflect/scala/reflect/internal/Symbols.scala
+++ src/reflect/scala/reflect/internal/Symbols.scala
@@ -3017,7 +3017,7 @@ trait Symbols extends api.Symbols { self: SymbolTable =>
       res
     }
 
-    override def isVarargs: Boolean = definitions.isVarArgsList(paramss.flatten)
+    override def isVarargs: Boolean = lastSuch(paramss)(!_.isEmpty).exists(definitions.isVarArgsList)
 
     override def returnType: Type = {
       def loop(tpe: Type): Type =
